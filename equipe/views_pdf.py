a09197a7e535d10d951278ab52c251db3ee41809
jonatas.simoes <jonatas.simoes@camtecnologia.com.br>
2025-02-15 15:08:31 -0300

    Added month name to PDF export and updated template to display it

--- equipe/views_pdf.py
+++ equipe/views_pdf.py
@@ -12,10 +12,28 @@ def exportar_tabela_para_pdf(request, equipe_pk):
     if request.method == 'POST':
         equipe = get_object_or_404(Equipe, pk=equipe_pk)
         ano_atual = datetime.now().year
+        
+        
+        # Dicionário de nomes de meses
+        meses = {
+            1: 'Janeiro',
+            2: 'Fevereiro',
+            3: 'Março',
+            4: 'Abril',
+            5: 'Maio',
+            6: 'Junho',
+            7: 'Julho',
+            8: 'Agosto',
+            9: 'Setembro',
+            10: 'Outubro',
+            11: 'Novembro',
+            12: 'Dezembro'
+        }
 
         # Capturando o mês a partir do POST
         mes_escolhido = request.POST.get('mes')
         mes = int(mes_escolhido) if mes_escolhido else datetime.now().month
+        mes_nome = meses[mes]
 
         # Decidir o ano baseado no mês selecionado
         ano = ano_atual if mes >= datetime.now().month else ano_atual + 1
@@ -31,7 +49,7 @@ def exportar_tabela_para_pdf(request, equipe_pk):
         escalas = Escala.objects.filter(funcao__equipe_id=equipe_pk, evento__data_inicio__gte=inicio_mes, evento__data_inicio__lte=fim_mes).order_by('evento__data_inicio')
 
         # Renderizar o HTML
-        html_string = render_to_string('meu_template.html', {'escalas': escalas, 'equipe': equipe})
+        html_string = render_to_string('meu_template.html', {'escalas': escalas, 'equipe': equipe, 'mes_nome': mes_nome})
 
         # Criar um objeto HTML WeasyPrint
         html = HTML(string=html_string)
