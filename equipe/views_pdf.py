9ff8c6f52ebd42d008c4854b9271cd32244428c8
jonatas.simoes <jonatas.simoes@camtecnologia.com.br>
2025-02-15 13:23:48 -0300

    Added modal for exporting PDF and modified views_pdf.py to handle POST requests for exporting PDF with specific month and year

--- equipe/views_pdf.py
+++ equipe/views_pdf.py
@@ -3,30 +3,48 @@ from django.shortcuts import get_object_or_404
 from .models import Equipe
 from escala.models import Escala
 from django.utils.timezone import now
-
 from django.template.loader import render_to_string
 from weasyprint import HTML
+from datetime import datetime, timedelta
+from django.utils.timezone import make_aware
 
 def exportar_tabela_para_pdf(request, equipe_pk):
-    # Buscar dados da tabela
-    equipe = get_object_or_404(Equipe, pk=equipe_pk)
-    # escalas = Escala.objects.filter(funcao__equipe=equipe, evento__data_inicio__gte=now()).order_by('evento__data_inicio')
-    escalas = Escala.objects.filter(funcao__equipe_id=equipe_pk).order_by('evento__data_inicio')
+    if request.method == 'POST':
+        equipe = get_object_or_404(Equipe, pk=equipe_pk)
+        ano_atual = datetime.now().year
 
-    # for i in escalas:
-    #     print(i) 
+        # Capturando o mês a partir do POST
+        mes_escolhido = request.POST.get('mes')
+        mes = int(mes_escolhido) if mes_escolhido else datetime.now().month
 
-    # Renderizar o HTML
-    html_string = render_to_string('meu_template.html', {'escalas': escalas})
-    
-    # Criar um objeto HTML WeasyPrint
-    html = HTML(string=html_string)
+        # Decidir o ano baseado no mês selecionado
+        ano = ano_atual if mes >= datetime.now().month else ano_atual + 1
 
-    # Gerar o PDF
-    pdf = html.write_pdf()
+        # Determinar o início e o fim do mês selecionado
+        inicio_mes = make_aware(datetime(ano, mes, 1))
+        if mes == 12:
+            fim_mes = make_aware(datetime(ano + 1, 1, 1)) - timedelta(seconds=1)
+        else:
+            fim_mes = make_aware(datetime(ano, mes + 1, 1)) - timedelta(seconds=1)
 
-    # Criar uma resposta HTTP com o PDF
-    response = HttpResponse(pdf, content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="minha_tabela.pdf"'
+        # Filtrar escalas pelo mês e ano
+        escalas = Escala.objects.filter(funcao__equipe_id=equipe_pk, evento__data_inicio__gte=inicio_mes, evento__data_inicio__lte=fim_mes).order_by('evento__data_inicio')
 
-    return response
+        # Renderizar o HTML
+        html_string = render_to_string('meu_template.html', {'escalas': escalas, 'equipe': equipe})
+
+        # Criar um objeto HTML WeasyPrint
+        html = HTML(string=html_string)
+
+        # Gerar o PDF
+        pdf = html.write_pdf()
+
+        # Nome do arquivo personalizado com nome da equipe e mês
+        nome_mes = inicio_mes.strftime('%B').capitalize()
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="escala_{equipe.nome}_{nome_mes}_{ano}.pdf"'
+
+        return response
+    else:
+        # Retorna alguma resposta para método GET ou outro método não suportado
+        return HttpResponse("Método não suportado", status=405)
