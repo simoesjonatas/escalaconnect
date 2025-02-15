7aef10b48f8ef40ea2530b7efc68989bc3657704
jonatas.simoes <jonatas.simoes@camtecnologia.com.br>
2025-02-15 12:34:12 -0300

    Added username generation based on first and last name in signup form, updated templates and forms to reflect changes.

--- usuario/forms_signup.py
+++ usuario/forms_signup.py
@@ -1,8 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
+from django.core.exceptions import ValidationError
+
 
 class SignupForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    last_name = forms.CharField(
+        max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
     telefone = forms.CharField(
         max_length=255, required=False, widget=forms.TextInput(attrs={'class': 'form-control'})
     )
@@ -26,10 +34,34 @@ class SignupForm(UserCreationForm):
 
     class Meta:
         model = Usuario
-        fields = ['username', 'email', 'password1', 'password2', 'telefone', 'aniversario', 'batismo', 'cpf']
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2', 'telefone', 'aniversario', 'batismo', 'cpf']
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        if first_name and last_name:
+            cleaned_data['username'] = f"{first_name}.{last_name}".lower().replace(' ', '')
+    
+        username = f"{first_name}.{last_name}".lower().replace(' ', '')
+        if Usuario.objects.filter(username=username).exists():
+            self.add_error(None, f'O nome de usuário "{username}" já está em uso.')
+            # raise ValidationError("Um usuário com este nome de usuário já existe.")
+
+        return cleaned_data
+
 
     def clean_aceitar_termos(self):
         """Valida se o usuário aceitou os termos."""
         if not self.cleaned_data.get('aceitar_termos'):
             raise forms.ValidationError("Você deve aceitar os Termos de Uso para se cadastrar.")
         return self.cleaned_data['aceitar_termos']
+    
+    def save(self, commit=True):
+        user = super(SignupForm, self).save(commit=False)
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        user.username = f"{first_name}.{last_name}".lower().replace(' ', '')
+        if commit:
+            user.save()
+        return user
