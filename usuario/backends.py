7248c672581e3fb7c0800f12c047b06c34c69c69
jonatas.simoes <jonatas.simoes@camtecnologia.com.br>
2025-03-12 17:29:01 -0300

    Added option to login with username or email in login form and updated backend to authenticate with either username or email.

--- usuario/backends.py
+++ usuario/backends.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 class CustomBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         User = get_user_model()
         try:
-            user = User.objects.get(username=username)
+            # busca o usuario por nome de usuario ou e-mail
+            user = User.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+            # user = User.objects.get(username=username)
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
         except User.DoesNotExist:
